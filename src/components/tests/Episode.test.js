154443656562d3f6493c5cad6841b526f2980e4e
Austin Johnson <austintjohnson92@gmail.com>
2022-04-25 19:47:31 -0700

    Completed episode tests

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,10 +3,30 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { 
-    render(<Episode episode={[]}/>)
+const episodePropsTest = {
+    summary: "This is the summary",
+    season: 1,
+    id: 553946
+}
+
+test("renders without error", () => {
+    render(<Episode episode={[]} />)
 });
 
-test("renders the summary test passed as prop", () => { });
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={episodePropsTest} />)
 
-test("renders default image when image is not defined", () => { });
+    expect(screen.getByText(/This is the summary/)).toBeVisible();
+    expect(screen.getByText(/This is the summary/)).not.toBeNull();
+    const { rerender, getByText } = render(<Episode episode={{ summary: 'This is a second summary' }} />);
+    rerender(<Episode episode={{ summary: 'This is a second summary' }} />)
+    // .rerender(<Episode episode={{summary: 'This is a second summary'}} />)
+    expect(getByText('This is a second summary')).toBeVisible()
+
+});
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={{ ...episodePropsTest, image: null }} />);
+    expect(screen.getByAltText("https://i.ibb.co/2FsfXqM/stranger-things.png")).toBeInTheDocument();
+});
+
